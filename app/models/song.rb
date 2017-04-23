075798352e2cb25610b38e8bef77d66515c29c3f
Andrew Fischer <afischer15@mac.com>
2017-04-23 17:12:42 -0400

    Fonts yay

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,12 @@ class Song < ActiveRecord::Base
   def upvote
     self.score += 1
     save
+    redirect_to @party, notice: 'Upvoted!'
   end
 
   def downvote
     self.score -= 1
     save
+    redirect_to @party, notice: 'Downvoted!'
   end
 end
